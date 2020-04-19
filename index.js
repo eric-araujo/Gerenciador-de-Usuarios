235ed447a885772c385b1aef6002126bfa39816f
eric-araujo <eric.araujolima@hotmail.com>
2020-04-19 16:22:08 -0300

    Adicionado função addLine

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(dataUser){
+
+    var tr = document.createElement("tr");
+
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+    document.querySelector("#table-users").appendChild(tr);
+
+}
+
 document.querySelector("#form-user-create").addEventListener("submit", (event) => {
 
     event.preventDefault();
@@ -8,14 +30,21 @@ document.querySelector("#form-user-create").addEventListener("submit", (event) =
     fields.forEach((field, index) => {
 
         if (field.name == "gender") {
+
             if (field.checked) {
+        
                 user[field.name] = field.value;
+        
             }
+        
         } else {
+        
             user[field.name] = field.value;
+        
         }
+    
     });
 
-    console.log(user);
+    addLine(user);
 
 });
