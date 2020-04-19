c75272b1ad6e4df5273f9fdd2fc2401cc340d929
eric-araujo <eric.araujolima@hotmail.com>
2020-04-19 16:00:41 -0300

    Adicionado função prenvetDefault

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach((field, index) => {
+document.querySelector("#form-user-create").addEventListener("submit", (event) => {
 
-    if (field.name == "gender") {
-        if(field.checked){
+    event.preventDefault();
+
+    fields.forEach((field, index) => {
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
             user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
-});
+    });
 
-console.log(user);
+    console.log(user);
+
+});
