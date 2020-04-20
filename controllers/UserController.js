2bf042c29a4245c1747656e7fb9a589bcee818c9
eric-araujo <eric.araujolima@hotmail.com>
2020-04-20 16:15:15 -0300

    Adicionado validação no formulário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -87,8 +87,15 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach((field) => {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if (field.name == "gender") {
 
@@ -109,6 +116,10 @@ class UserController {
             }
 
         });
+        
+        if(!isValid){
+            return false;
+        }
 
         return new User(
             user.name,
