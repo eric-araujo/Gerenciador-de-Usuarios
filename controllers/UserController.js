5774a91f14252b5d7531c987d9616facc35a17e2
eric-araujo <eric.araujolima@hotmail.com>
2020-04-22 17:42:04 -0300

    Armazenando dados do usuário no localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -219,9 +219,9 @@ class UserController {
 
         let users = [];
 
-        if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
 
@@ -251,7 +251,8 @@ class UserController {
 
         users.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(users));
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
 
     }
 
