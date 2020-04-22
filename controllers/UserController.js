f82b24fa423af558e7ed718f9095ff64bc97fefa
eric-araujo <eric.araujolima@hotmail.com>
2020-04-22 17:36:35 -0300

    Armazenando os dados na sessionStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
 
     }
 
@@ -44,7 +45,7 @@ class UserController {
 
                         result._photo = userOld._photo
 
-                    } else { 
+                    } else {
 
                         result._photo = content;
 
@@ -104,6 +105,8 @@ class UserController {
                 (content) => {
 
                     values.photo = content;
+
+                    this.insert(values);
 
                     this.addLine(values);
 
@@ -212,6 +215,46 @@ class UserController {
 
     }
 
+    getUsersStorage() {
+
+        let users = [];
+
+        if (sessionStorage.getItem("users")) {
+
+            users = JSON.parse(sessionStorage.getItem("users"));
+
+        }
+
+        return users;
+
+    }
+
+    selectAll() {
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+
+        });
+
+    }
+
+    insert(data) {
+
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        sessionStorage.setItem("users", JSON.stringify(users));
+
+    }
+
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
@@ -242,7 +285,7 @@ class UserController {
 
         tr.querySelector(".btn-delete").addEventListener("click", (event) => {
 
-            if(confirm("Deseja realmente excluir?")){
+            if (confirm("Deseja realmente excluir?")) {
 
                 tr.remove();
 
--- models/User.js
+++ models/User.js
@@ -74,4 +74,21 @@ class User {
 
     }
 
+    loadFromJSON(json) {
+
+        for (let name in json) {
+
+            switch(name){
+
+                case '_register':
+                    this[name] = new Date(json[name]);
+                break;
+                default:
+                    this[name] = json[name];
+            }
+
+        }
+
+    }
+
 }
