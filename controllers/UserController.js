3cc564d0e69f2b87cd0cde6f99d633c3da27e23c
eric-araujo <eric.araujolima@hotmail.com>
2020-04-22 14:16:36 -0300

    Editando os dados do usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,20 +1,55 @@
 class UserController {
 
-    constructor(formId, tableId) {
+    constructor(formIdCreate, formIdUpdate, tableId) {
 
-        this.formEl = document.getElementById(formId);
+        this.formEl = document.getElementById(formIdCreate);
+        this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
-        this.onEditCancel();
+        this.onEdit();
 
     }
 
-    onEditCancel() {
+    onEdit() {
 
         document.querySelector("#box-user-update .btn-cancel").addEventListener("click", (event) => {
 
             this.showPanelCreate();
+
+        });
+
+        this.formUpdateEl.addEventListener("submit", (event) => {
+
+            event.preventDefault();
+
+            let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues(this.formUpdateEl);
+
+            let index = this.formUpdateEl.dataset.trIndex;
+
+            let tr = this.tableEl.rows[index];
+
+            tr.dataset.user = JSON.stringify(values);
+
+            tr.innerHTML = `
+                <td><img src="${values.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${values.name}</td>
+                <td>${values.email}</td>
+                <td>${(values.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(values.register)}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            `;
+
+            this.addEventsTr(tr);
+
+            this.updateCount();
 
         });
 
@@ -30,7 +65,7 @@ class UserController {
 
             btn.disabled = true;
 
-            let values = this.getValues();
+            let values = this.getValues(this.formEl);
 
             if (!values) return false;
 
@@ -97,12 +132,12 @@ class UserController {
     }
 
 
-    getValues() {
+    getValues(formEl) {
 
         let user = {};
         let isValid = true;
 
-        [...this.formEl.elements].forEach((field) => {
+        [...formEl.elements].forEach((field) => {
 
             if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
@@ -165,10 +200,22 @@ class UserController {
             </td>
         `;
 
+        this.addEventsTr(tr);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+    }
+
+    addEventsTr(tr){
+
         tr.querySelector(".btn-edit").addEventListener("click", (event) => {
 
             let json = JSON.parse(tr.dataset.user);
             let form = document.querySelector("#form-user-update");
+
+            form.dataset.trIndex = tr.sectionRowIndex;
 
             for (let name in json) {
 
@@ -181,16 +228,16 @@ class UserController {
                     switch (field.type) {
                         case 'file':
                             continue;
-                        break;
+                            break;
 
                         case 'radio':
                             field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
                             field.checked = true;
-                        break;
+                            break;
 
                         case 'checkbox':
                             field.checked = json[name];
-                        break;
+                            break;
 
                         default:
                             field.value = json[name];
@@ -203,10 +250,6 @@ class UserController {
             this.showPanelUpdate();
 
         });
-
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
 
     }
 
